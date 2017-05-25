0c91d03b5aaf15b558470663528c31ed06cbfba2
trainer7 <trainer7@trainer7s-MacBook-Pro.local>
2017-05-25 08:55:57 -0600

    Eureka added to events and shows

--- episodic-events/src/main/java/com/example/episodicevents/DemoApplication.java
+++ episodic-events/src/main/java/com/example/episodicevents/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.episodicevents;
 import com.example.episodicevents.events.FastForwardEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
